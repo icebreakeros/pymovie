15e581bbc13499dd74fbf110540e5a9cca1871cf
icebrakeros <rpioshome@gmail.com>
2019-02-25 12:02:54 -0500

    Gathering IMDb records in lists and printing first 25 records

--- pymovie.py
+++ pymovie.py
@@ -1,7 +1,18 @@
 from bs4 import BeautifulSoup
+import pandas as pd
 import lxml
 import requests
 import re
+
+
+# Variables that we are going to use
+i = 0
+N = 25 # number of lines to print
+place_lst = []
+rating_lst = []
+title_lst = []
+year_lst = []
+starring_lst = []
 
 # Download IMDB's Top 250 data
 url = 'http://www.imdb.com/chart/top'
@@ -34,5 +45,26 @@ for index in range(0, len(movies)):
             "link": links[index]}
     imdb.append(data)
 
+'''
+#Trying to use Pandas DataFrame
+    test_df = pd.DataFrame({"movie_title": movie_title,
+                            "rating": round(float(ratings[index]), 2),
+                            "year": year,
+                            "star_cast": crew[index],
+                            "place": place})
+
+print (test_df.info())
+'''
+
 for item in imdb:
-    print(item['place'], '*', round(float(item['rating']), 2), '*', item['movie_title'], '('+item['year']+') *', 'Starring:', item['star_cast'])
+    place_lst.append(item['place'])
+    rating_lst.append(round(float(item['rating']), 1))
+    title_lst.append(item['movie_title'])
+    year_lst.append(item['year'])
+    starring_lst.append(item['star_cast'])
+
+for i in range(N):
+    print (place_lst[i] + (4-len(place_lst[i]))*" " + str(rating_lst[i]) + (4-len(str(rating_lst[i])))*" " +\
+           title_lst[i] + (50 - len(title_lst[i])) * " " + year_lst[i] + 4*" " +  starring_lst[i])
+
+#    print(item['place'], '*', round(float(item['rating']), 2), '*', item['movie_title'], '('+item['year']+') *', 'Starring:', item['star_cast'])
